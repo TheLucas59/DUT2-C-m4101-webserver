ae9556db6632af86931668d097cb13a589d7c86b
Loïc Demay <loic.demay.etu@univ-lille.fr>
2021-02-03 11:46:29 +0100

    check socket / indents / returns socket.c

--- webserver/main.c
+++ webserver/main.c
@@ -11,74 +11,78 @@
 #define BUFF_LENGTH 50
 
 void traitement_signal(int sig) {
-  printf("Signal %d reçu\n", sig);
+	printf("Signal %d reçu\n", sig);
 
-  int pid = waitpid(-1, NULL, 0);
-  printf("Le processus %d a été arrêté\n", pid);
+	int pid = waitpid(-1, NULL, WNOHANG);
+	printf("Le processus %d a été arrêté\n", pid);
 }
 
 void initialiser_signaux(void){
-  if(signal(SIGPIPE, SIG_IGN) == SIG_ERR){
-    perror("Signal SIGPIPE");
-  }
+	if(signal(SIGPIPE, SIG_IGN) == SIG_ERR){
+		perror("Signal SIGPIPE");
+	}
 
-  struct sigaction sa;
+	struct sigaction sa;
 
-  sa.sa_handler = traitement_signal;
-  sigemptyset(&sa.sa_mask);
-  sa.sa_flags = SA_RESTART;
-  
-  if(sigaction(SIGCHLD, &sa, NULL) == -1) {
-    perror("sigaction(SIGCHLD)");
-  }
-	      
+	sa.sa_handler = traitement_signal;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+
+	if(sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction(SIGCHLD)");
+	}
+			
 }
 
 
 int main(int argc, char** argv) {
-  if(argc > 1 && strcmp(argv[1], "-advice") == 0) {
-    printf("Don't panic\n");
-    return 42;
-  }
-  printf("Need an advice ?\n");
+	if(argc > 1 && strcmp(argv[1], "-advice") == 0) {
+		printf("Don't panic\n");
+		return 42;
+	}
+	printf("Need an advice ?\n");
 
-  initialiser_signaux();
+	initialiser_signaux();
 
-  int socket_serveur = creer_serveur(PORT);
-  int socket_client;
-  while(1) {
-    int rad;
-    char* buff[BUFF_LENGTH];
-    
-    socket_client = accept(socket_serveur, NULL, NULL);
-    if(fork() == 0) {
-      if(socket_client == -1) {
-	perror("accept socket client");
-      }
+	int socket_serveur = creer_serveur(PORT);
+	if(socket_serveur == -1){
+		perror("créer serveur");
+		return -1;
+	}
+	int socket_client;
+	while(1) {
+		int rad;
+		char* buff[BUFF_LENGTH];
+		
+		socket_client = accept(socket_serveur, NULL, NULL);
+		if(fork() == 0) {
+			if(socket_client == -1) {
+				perror("accept socket client");
+			}
 
-      printf("Connexion établie\n");
-      
-      sleep(1);
-      const char* message_bienvenue = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc neque nisl, tempus sed elementum dictum, fringilla in massa. Nam tempor massa lacus, non posuere mi mattis vitae. Fusce velit orci, efficitur at sodales et, luctus vel felis. Duis viverra, lorem at congue dignissim, elit elit vestibulum urna, fringilla euismod ante mi a leo. Praesent at nulla et nunc pulvinar congue in ut felis. Proin ac dui ut neque finibus vulputate non sed arcu. Aenean suscipit nisl nec tempor accumsan. Praesent at odio finibus, accumsan ipsum non, varius lacus. Nullam tristique mauris in sem imperdiet hendrerit. Proin pharetra accumsan urna iaculis malesuada. In dictum neque ac gravida convallis. Ut quis arcu eu velit tincidunt iaculis ac et sem. Vivamus et velit justo. Etiam at massa tristique, aliquet eros egestas, finibus nunc. Maecenas nulla augue, mattis at ornare in, aliquet eget massa. Suspendisse fermentum ex at libero molestie, quis lacinia nulla congue. Vivamus maximus orci nisl, at vestibulum risus molestie in. In hac habitasse platea dictumst. Maecenas justo sapien, aliquet sed nunc id, finibus maximus odio. Vestibulum non mauris ante. Duis bibendum fermentum lacus sit amet suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc quis velit venenatis, congue neque non, finibus tellus. Nulla nec ullamcorper nunc, id dapibus enim.\n";
-      write(socket_client, message_bienvenue, strlen(message_bienvenue));
-    
-      while((rad = read(socket_client, buff, BUFF_LENGTH)) != 0) {
-	write(socket_client, buff, rad);
-      }
-      
-      if(rad == 0) {
-	printf("Connexion coupée\n");
-      }
-      else if(rad == -1) {
-	perror("read");
-      }
-    
-      close(socket_client);
-      exit(0);
-    }
-    close(socket_client);
-  }
-  close(socket_serveur);
-  return 0;
+			printf("Connexion établie\n");
+			
+			sleep(1);
+			const char* message_bienvenue = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc neque nisl, tempus sed elementum dictum, fringilla in massa. Nam tempor massa lacus, non posuere mi mattis vitae. Fusce velit orci, efficitur at sodales et, luctus vel felis. Duis viverra, lorem at congue dignissim, elit elit vestibulum urna, fringilla euismod ante mi a leo. Praesent at nulla et nunc pulvinar congue in ut felis. Proin ac dui ut neque finibus vulputate non sed arcu. Aenean suscipit nisl nec tempor accumsan. Praesent at odio finibus, accumsan ipsum non, varius lacus. Nullam tristique mauris in sem imperdiet hendrerit. Proin pharetra accumsan urna iaculis malesuada. In dictum neque ac gravida convallis. Ut quis arcu eu velit tincidunt iaculis ac et sem. Vivamus et velit justo. Etiam at massa tristique, aliquet eros egestas, finibus nunc. Maecenas nulla augue, mattis at ornare in, aliquet eget massa. Suspendisse fermentum ex at libero molestie, quis lacinia nulla congue. Vivamus maximus orci nisl, at vestibulum risus molestie in. In hac habitasse platea dictumst. Maecenas justo sapien, aliquet sed nunc id, finibus maximus odio. Vestibulum non mauris ante. Duis bibendum fermentum lacus sit amet suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc quis velit venenatis, congue neque non, finibus tellus. Nulla nec ullamcorper nunc, id dapibus enim.\n";
+			write(socket_client, message_bienvenue, strlen(message_bienvenue));
+			
+			while((rad = read(socket_client, buff, BUFF_LENGTH)) != 0) {
+				write(socket_client, buff, rad);
+			}
+			
+			if(rad == 0) {
+				printf("Connexion coupée\n");
+			}
+			else if(rad == -1) {
+				perror("read");
+			}
+			
+			close(socket_client);
+			exit(0);
+		}
+		close(socket_client);
+	}
+	close(socket_serveur);
+	return 0;
 }
 
--- webserver/socket.c
+++ webserver/socket.c
@@ -6,33 +6,36 @@
 #define BACKLOG 10
 
 int creer_serveur(int port) {
-  int socket_serveur;
+	int socket_serveur;
 
-  socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 
-  if(socket_serveur == -1) {
-    perror("socket_serveur");
-  }
+	if(socket_serveur == -1) {
+		perror("socket_serveur");
+		return -1;
+	}
 
-  struct sockaddr_in saddr;
-  saddr.sin_family = AF_INET;
-  saddr.sin_port = htons(port);
-  saddr.sin_addr.s_addr = INADDR_ANY;
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(port);
+	saddr.sin_addr.s_addr = INADDR_ANY;
 
-  int optval = 1;
+	int optval = 1;
 
-  if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1) {
-    perror("Can not set SO_REUSEADDR option");
-  }
+	if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1) {
+		perror("Can not set SO_REUSEADDR option");
+		return -1;
+	}
 
-  if(bind(socket_serveur, (struct sockaddr*) &saddr, sizeof(saddr)) == -1) {
-    perror("bind serveur socket");
-  }
+	if(bind(socket_serveur, (struct sockaddr*) &saddr, sizeof(saddr)) == -1) {
+		perror("bind serveur socket");
+		return -1;
+	}
 
-  if(listen(socket_serveur, BACKLOG) == -1) {
-    perror("listen serveur socket");
-  }
+	if(listen(socket_serveur, BACKLOG) == -1) {
+		perror("listen serveur socket");
+		return -1;
+	}
 
-  return socket_serveur;
-  
+	return socket_serveur;
 }
