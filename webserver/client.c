a9aef8a7b4c8aeff485f1b18e8dd6dae439a2641
lucas plé <lucas.ple.etu@univ-lille.fr>
2021-02-17 09:50:58 +0100

    fgets_or_exit

--- webserver/client.c
+++ webserver/client.c
@@ -1,23 +1,29 @@
 #include "socket.h"
 #include "client.h"
+#include "../parselib/http_parse.h"
 #define BUFF_LENGTH 50
 
 int analyser_ligne_1(char* buff) {
     return strncmp("GET / HTTP/1.1\r\n", buff, 16);
 }
 
+char* fgets_or_exit(char* buffer, int size, FILE* stream) {
+    if(fgets(buffer, size, stream) == NULL) {
+        exit(1);
+    }
+    return buffer;
+ }
+
+
 void traitement_client(int socket_client, char* buff, FILE* fichier) {
     int i = 1;
     if(socket_client == -1) {
 		perror("accept socket client");
-    }
+}
 
 	printf("Connexion établie\n");
 			
-	sleep(1);
-	const char* message_bienvenue = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc neque nisl, tempus sed elementum dictum, fringilla in massa. Nam tempor massa lacus, non posuere mi mattis vitae. Fusce velit orci, efficitur at sodales et, luctus vel felis. Duis viverra, lorem at congue dignissim, elit elit vestibulum urna, fringilla euismod ante mi a leo. Praesent at nulla et nunc pulvinar congue in ut felis. Proin ac dui ut neque finibus vulputate non sed arcu. Aenean suscipit nisl nec tempor accumsan. Praesent at odio finibus, accumsan ipsum non, varius lacus. Nullam tristique mauris in sem imperdiet hendrerit. Proin pharetra accumsan urna iaculis malesuada. In dictum neque ac gravida convallis. Ut quis arcu eu velit tincidunt iaculis ac et sem. Vivamus et velit justo. Etiam at massa tristique, aliquet eros egestas, finibus nunc. Maecenas nulla augue, mattis at ornare in, aliquet eget massa. Suspendisse fermentum ex at libero molestie, quis lacinia nulla congue. Vivamus maximus orci nisl, at vestibulum risus molestie in. In hac habitasse platea dictumst. Maecenas justo sapien, aliquet sed nunc id, finibus maximus odio. Vestibulum non mauris ante. Duis bibendum fermentum lacus sit amet suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc quis velit venenatis, congue neque non, finibus tellus. Nulla nec ullamcorper nunc, id dapibus enim.\n";
-		
-	while(fgets(buff, BUFF_LENGTH, fichier) != NULL) {
+	while(fgets_or_exit(buff, BUFF_LENGTH, fichier)) {
         if(i == 1) {
             if(strstr(buff, "inexistant") != NULL) {
                 fprintf(fichier, "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-Length: 15\r\n\r\n404 Not found\r\n");
--- webserver/client.h
+++ webserver/client.h
@@ -1,1 +1,14 @@
 void traitement_client(int socket_client, char* buff, FILE* fichier);
+char* fgets_or_exit(char* buffer, int size, FILE* stream);
+
+enum http_method {
+    HTTP_GET,
+    HTTP_UNSUPPORTED,
+};
+
+typedef struct {
+    enum http_method method;
+    int major_version;
+    int minor_version;
+    char target[MAX_TARGET_SIZE];
+} http_request;
