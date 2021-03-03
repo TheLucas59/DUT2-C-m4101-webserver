3f056678c19f6b840b1ffb1ac2494d86fffa1beb
lucas plé <lucas.ple.etu@univ-lille.fr>
2021-03-03 11:48:30 +0100

    chapitre 7 pas fini

--- webserver/client.c
+++ webserver/client.c
@@ -28,6 +28,59 @@ void send_response(FILE* client, int code, const char* reason_phrase, const char
     fprintf(client, "%s", message_body);
 }
 
+char* rewrite_target(char* target) {
+    int i = 0;
+    char* res = (char*) malloc(strlen(target));
+    while(target[i] != '\0' || target[i] != '?') {
+        res[i] = target[i];
+        i++;
+    }
+    if(target[i] == '?') {
+        res[i] = '\0';
+    }
+    return res;
+}
+
+FILE* check_and_open(const char *target, const char *document_root) {
+    int targetLength = strlen(target);
+    int document_rootLength = strlen(document_root);
+    char path[targetLength + document_rootLength + 1];
+    strcpy(path, document_root);
+    strcat(path, target);
+
+    struct stat stats;
+    stat(path, &stats);
+    
+    if((stats.st_mode & S_IFMT) != S_IFREG) {
+        perror("Ce n'est pas un fichier régulier.");
+        return NULL;
+    }
+
+    FILE* file = fopen(path, "r");
+    return file;
+}
+
+int get_file_size(FILE* file) {
+    struct stat stats;
+    int fd = fileno(file);
+    fstat(fd, &stats);
+    return stats.st_size;
+}
+
+int copy(FILE *in, FILE *out) {
+    char buff[get_file_size(in)];
+    if(read(fileno(in), buff, get_file_size(in)) == -1) {
+        perror("Erreur de lecture du fichier");
+        return -1;
+    }
+    if(write(fileno(out), buff, get_file_size(in)) == -1) {
+        perror("Erreur de copie du fichier");
+        return -1;
+    }
+
+    return 0;
+}
+
 void traitement_client(int socket_client, char* buff, FILE* client) {
     if(socket_client == -1) {
 		perror("accept socket client");
@@ -42,16 +95,29 @@ void traitement_client(int socket_client, char* buff, FILE* client) {
     fgets_or_exit(buff, BUFF_LENGTH, client);
     int parse = parse_http_request(buff, &req);
     skip_headers(client);
+    FILE* fichier = NULL;
+    char* target;
     if(parse == -1) {
-        if(req.method == HTTP_UNSUPPORTED) 
+        target = rewrite_target(req.target);
+        fichier = check_and_open(target, "../");
+        //int size;
+        //size = get_file_size(fichier);
+        if(req.method == HTTP_UNSUPPORTED) {
             send_response(client, 405, "Method Not Allowed", "Method Not Allowed\r\n");
-        else
+        }
+        else {
             send_response(client, 400, "Bad Request", "Bad Request\r\n");
+        }
     } 
-    else if (strcmp(req.target, "/") == 0)
+    else if (strcmp(req.target, "/") == 0) {
         send_response(client, 200, "OK", bvn);
-    else
+        if(copy(fichier, client) == -1) {
+            perror("Erreur de transmission du fichier");
+            exit(1);
+        }
+    }
+    else {
         send_response(client, 404, "Not Found", "Not Found\r\n");
-
+    }
     exit(0);
 }
